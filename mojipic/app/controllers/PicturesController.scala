56dfc5e092e2102d105c3205e229acefbad78c0f
Kento Okamura <kentooooo.1230@gmail.com>
2018-01-06 01:04:40 +0900

    MySQL,Redisにデータを保存できるようにした

--- mojipic/app/controllers/PicturesController.scala
+++ mojipic/app/controllers/PicturesController.scala
@@ -5,39 +5,46 @@ import java.time.{Clock, LocalDateTime}
 import javax.inject.{Inject, Singleton}
 
 import com.google.common.net.MediaType
+import com.redis.RedisClient
 import domain.entity.{PictureProperty, TwitterId}
+import domain.repository.PicturePropertyRepository
+import infrastructure.redis.RedisKeys
 import play.api.cache.SyncCacheApi
 import play.api.libs.Files.TemporaryFile
-import play.api.mvc.{ControllerComponents, MultipartFormData}
+import play.api.mvc._
 import play.api.mvc.MultipartFormData.FilePart
 
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class PicturesController @Inject()(
-                                 cc: ControllerComponents,
-                                 clock: Clock,
-                                 executionContext: ExecutionContext,
-                                 val cache:SyncCacheApi
-                                 ) extends TwitterLoginController(cc) {
+                                    cc: ControllerComponents,
+                                    clock: Clock,
+                                    executionContext: ExecutionContext,
+                                    val cache: SyncCacheApi,
+                                    picturePropertyRepository: PicturePropertyRepository,
+                                    redisClient: RedisClient
+                                  ) extends TwitterLoginController(cc) {
 
-  implicit  val ec = executionContext
+  implicit val ec = executionContext
   val originalStoreDirPath = "./filesystem/original"
 
   def post = TwitterLoginAction.async { request =>
-    (request.accessToken,request.body.asMultipartFormData) match {
-      case(Some(accessToken),Some(form)) =>
+    (request.accessToken, request.body.asMultipartFormData) match {
+      case (Some(accessToken), Some(form)) =>
         form.file("file") match {
           case Some(file) =>
             val storeDirPath = FileSystems.getDefault.getPath(originalStoreDirPath)
             if (!Files.exists(storeDirPath)) Files.createDirectories(storeDirPath)
 
-            val originalFilepath = FileSystems.getDefault.getPath(storeDirPath.toString,System.currentTimeMillis().toString)
-            Files.copy(file.ref.path,originalFilepath,StandardCopyOption.COPY_ATTRIBUTES)
+            val originalFilepath =  FileSystems.getDefault.getPath(storeDirPath.toString, System.currentTimeMillis().toString)
+            Files.copy(file.ref.path, originalFilepath, StandardCopyOption.COPY_ATTRIBUTES)
             val propertyValue = createPicturePropertyValue(TwitterId(accessToken.getUserId), file, form, originalFilepath)
-
-            println(propertyValue)
-          // TODO MySQLへのプロパティ保存とRedisへのタスクの保存
+            val pictureId = picturePropertyRepository.create(propertyValue)
+            pictureId.map({ (id) =>
+              redisClient.rpush(RedisKeys.Tasks, id.value)
+              Ok("Picture uploaded.")
+            })
             Future.successful(Ok("Picture uploaded."))
           case _ => Future.successful(Unauthorized("Need picture data."))
         }
@@ -46,12 +53,11 @@ class PicturesController @Inject()(
   }
 
   private[this] def createPicturePropertyValue(
-                                              twitterId: TwitterId,
-                                              file: FilePart[TemporaryFile],
-                                              form: MultipartFormData[TemporaryFile],
-                                              originalFilePath: Path
+                                                twitterId: TwitterId,
+                                                file: FilePart[TemporaryFile],
+                                                form: MultipartFormData[TemporaryFile],
+                                                originalFilePath: Path
                                               ): PictureProperty.Value = {
-
     val overlayText = form.dataParts.get("overlaytext").flatMap(_.headOption).getOrElse("")
     val overlayTextSize = form.dataParts.get("overlaytextsize").flatMap(_.headOption).getOrElse("60").toInt
     val contentType = MediaType.parse(file.contentType.getOrElse("application/octet-stream"))
@@ -66,7 +72,6 @@ class PicturesController @Inject()(
       Some(originalFilePath.toString),
       None,
       LocalDateTime.now(clock))
-
   }
 
 }
